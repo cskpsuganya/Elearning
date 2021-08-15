103cc9aac2d95fbbdc5674b9b72fedc3ff74f58c
cskpsuganya <88704038+cskpsuganya@users.noreply.github.com>
2021-08-15 12:37:59 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,7 @@ axios.defaults.headers.common.Accept = 'application/json';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
+
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
