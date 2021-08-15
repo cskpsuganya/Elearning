e29eaed57eca8c3731e5f902a5339f8d55a63c5b
cskpsuganya <88704038+cskpsuganya@users.noreply.github.com>
2021-08-15 12:36:30 +0530

    Update index.js

--- frontend/src/app/landing/index.js
+++ frontend/src/app/landing/index.js
@@ -2,4 +2,4 @@ import LandingDetails from './landingDetails';
 import LandingDisplay from './landingDisplay';
 import LandingTitle from './landingTitle';
 
-export { LandingTitle, LandingDisplay, LandingDetails };
+export { LandingTitle, LandingDisplay, LandingDetails };/
